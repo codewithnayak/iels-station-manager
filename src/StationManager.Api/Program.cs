811054378c00a69c003f247ba736ef88dec542b1
codewithnayak <70228044+codewithnayak@users.noreply.github.com>
2026-07-26 17:44:52 +0100

    feat: code changes for pipeline

--- src/StationManager.Api/Program.cs
+++ src/StationManager.Api/Program.cs
@@ -3,6 +3,17 @@ using StationManager.Api.DI;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Load Kubernetes ConfigMap JSON if present
+var configMapPath = "/app/config/appsettings.json";
+
+if (File.Exists(configMapPath))
+{
+    builder.Configuration.AddJsonFile(configMapPath, optional: true, reloadOnChange: true);
+}
+
+builder.Configuration.AddEnvironmentVariables();
+
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
